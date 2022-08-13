98db732606865a80359d2dd2baf5d3182fb94c24
zhouzhicheng61 <zhouzhicheng61@gmail.com>
2022-08-13 08:32:39 +0800

    master

--- 前端/02.Part 2 · JavaScript 网页编程/Web APIs网页应用编程/DOM/commom.js
+++ 前端/02.Part 2 · JavaScript 网页编程/Web APIs网页应用编程/DOM/commom.js
@@ -1,3 +1,22 @@
+// 根据元素的id来获取元素
 function my$(id) {
     return document.getElementById(id);
 };
+
+// 给元素绑定事件
+function addEvent(ele, type, fun) {
+    if (ele.addEventListener) {
+        ele.addEventListener(type, fun);
+    } else if (ele.attachEvent) {
+        ele.attachEvent("on" + type, fun);
+    }
+}
+
+// 给元素注销事件
+function removeEvent(ele, type, fun) {
+    if (ele.removeEventListener) {
+        ele.addEventListener(type, fun);
+    } else if (ele.attachEvent) {
+        ele.detachEvent("on" + type, fun);
+    }
+}
