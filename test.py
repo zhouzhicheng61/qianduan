b0d162fae560fbabf6517ca91eecb73c8c5648e9
zhouzhicheng61 <zhouzhicheng61@gmail.com>
2022-09-18 19:40:14 +0800

    master

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#——————————————————99乘法表
+# ——————————————————99乘法表
 # i = 1
 # while i <= 9:
 #     j = 1
@@ -8,7 +8,7 @@
 #     print("")
 #     i += 1
 
-#——————————————————10以内的素数
+# ——————————————————10以内的素数
 # while 方法
 # i = 2
 # num = int(input("想查询多少以内的素数呢？"))
@@ -24,7 +24,7 @@
 #         j += 1
 #     i += 1
 
-# # for 方法
+# for 方法
 # num = int(input("想查询多少以内的素数呢？"))
 # for i in range(2, num):
 #     for j in range(2, i):
@@ -33,3 +33,16 @@
 #             break
 #     else:
 #         print(i, "是质数")
+
+# 获取从左下到右上对角线的值
+list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+n = len(list)
+a = []
+for i in list:
+    a.append(i[n - 1])
+    n -= 1
+print(a)
+
+b = [list[i][-i - 1] for i in range(len(list))]
+print(b)
