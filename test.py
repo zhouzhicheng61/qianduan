554e324e6134e84f3425c92ebc094314678aeddc
zhouzhicheng61 <1679673306@qq.com>
2023-01-18 21:39:45 +0800

    master

--- test.py
+++ test.py
@@ -1,29 +1,42 @@
 import pdfplumber
 import pandas as pd
 import jieba
+import os
 
-pdf = pdfplumber.open('/Users/zhouzhicheng61/Desktop/北京城市物流功能定位和发展目标.pdf')
-pages = pdf.pages
-txt = ""
-for each in pages:
-    txt += each.extract_text()
-words = jieba.lcut(txt)
-# 要排除的词汇
-excludes = {"北京"}
-counts = {}
-for word in words:
-    if len(word) == 1:
-        continue
-    # 把什么内容替换成什么内容
-    elif word == "物流中心":
-        rword = "物流"
-    else:
-        rword = word
-        counts[rword] = counts.get(rword, 0) + 1
-for word in excludes:
-    del counts[word]
-items = list(counts.items())
-items.sort(key=lambda x: x[1], reverse=True)
-for i in range(15):
-    word, count = items[i]
-    print("{0:<10}{1:>5}".format(word, count))
+
+def getWordsInCount(path, fileName):
+    pdf = pdfplumber.open(path + "\\" + fileName)
+    pages = pdf.pages
+    txt = ""
+    for each in pages:
+        txt += each.extract_text()
+    words = jieba.lcut(txt)
+    # 要排除的词汇
+    excludes = {}
+    counts = {}
+    for word in words:
+        if len(word) == 1:
+            continue
+        # 把什么内容替换成什么内容
+        elif word == "物流中心":
+            rword = "物流"
+        else:
+            rword = word
+            counts[rword] = counts.get(rword, 0) + 1
+    for rword in excludes:
+        del counts[rword]
+    items = list(counts.items())
+    items.sort(key=lambda x: x[1], reverse=True)
+    for i in range(15):
+        rword, count = items[i]
+        print("{0:<10}{1:>5}".format(rword, count))
+
+
+path = "F:\课题\北京20年物流发展\政策\国家（北京）物流产业政策\北京—2017"
+
+fileNames = []
+for i in os.walk(path):
+    fileNames = i[2]
+
+for j in range(len(fileNames)):
+    getWordsInCount(path, fileNames[j])
