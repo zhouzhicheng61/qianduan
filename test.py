ad837b88c9f940c266480a910bf9a67b5e90cf51
zhouzhicheng61 <1679673306@qq.com>
2023-01-18 23:33:15 +0800

    master

--- test.py
+++ test.py
@@ -2,41 +2,42 @@ import pdfplumber
 import pandas as pd
 import jieba
 import os
+import openpyxl
 
 
-def getWordsInCount(path, fileName):
+def getWordsInCount(year, level, field, path, fileName):
     pdf = pdfplumber.open(path + "\\" + fileName)
     pages = pdf.pages
     txt = ""
     for each in pages:
         txt += each.extract_text()
     words = jieba.lcut(txt)
-    # 要排除的词汇
-    excludes = {}
     counts = {}
     for word in words:
         if len(word) == 1:
             continue
-        # 把什么内容替换成什么内容
-        elif word == "物流中心":
-            rword = "物流"
         else:
             rword = word
-            counts[rword] = counts.get(rword, 0) + 1
-    for rword in excludes:
-        del counts[rword]
+        counts[rword] = counts.get(rword, 0) + 1
     items = list(counts.items())
     items.sort(key=lambda x: x[1], reverse=True)
-    for i in range(15):
-        rword, count = items[i]
-        print("{0:<10}{1:>5}".format(rword, count))
+    for i in range(10):
+        data = [year, level, field]
+        word, count = items[i]
+        data.append(fileName[:-4])
+        data.append(word)
+        data.append(count)
+        sheet.append(data)
+    wb.save("F:\课题\北京20年物流发展\数据\政策分析内容.xlsx")
 
 
 path = "F:\课题\北京20年物流发展\政策\国家（北京）物流产业政策\北京—2017"
-
+wb = openpyxl.load_workbook("F:\课题\北京20年物流发展\数据\政策分析内容.xlsx")
+sheet = wb['Sheet1']
 fileNames = []
+
 for i in os.walk(path):
     fileNames = i[2]
 
 for j in range(len(fileNames)):
-    getWordsInCount(path, fileNames[j])
+    getWordsInCount("2017", "国家", "物流", path, fileNames[j])
